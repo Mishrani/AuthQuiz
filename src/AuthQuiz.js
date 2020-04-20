e8057b3fd9e0a107f9f532021b63039dd58e09be
Nitesh Mishra <mishra.nitesh987@gmail.com>
2020-04-21 00:23:47 +0530

    author value undefined from index.js in Turn Component

--- src/AuthQuiz.js
+++ src/AuthQuiz.js
@@ -1,49 +1,68 @@
-import React from 'react';
-import './App.css';
-import './bootstrap.min.css'
+import React from "react";
+import "./App.css";
+import "./bootstrap.min.css";
 
 function Hero() {
-  return (<div className="row">
-    <div className="jumbotron col-10 offset-1">
-      <h1>Author Quiz</h1>
-      <p>Select the book written by the Author shown</p>
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the Author shown</p>
+      </div>
     </div>
-  </div>);
+  );
 }
-function Turn({author,books}) {
-  return <div className="row turn" style={{background: "white"}}>
-            <div className="col-4 offset-1">
-              <img src={author.imageUrl} className="authorimage" alt="Author"/>
-            </div>
-            <div className="col-6">
-              {books.map((title) => <Book title={title} key={title} />)}
-            </div>
-          </div>
+function Turn({ author, books }) {
+  console.log(author);
+
+  return (
+    <div className="row turn" style={{ background: "white" }}>
+      <div className="col-4 offset-1 authorimage">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.books.map((title) => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
 }
-function Book({title}){
-  return <div/>
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <div>{title}</div>
+    </div>
+  );
 }
 function Continue() {
-  return <div />
+  return <div />;
 }
 function Footer() {
-  return (<div id="footer" className="row">
-    <div className="col-12">
-      <p className="text-muted credit">
-        All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+  return (
+    <div id="footer" className="row turn">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from{" "}
+          <a href="http://commons.wikimedia.org/wiki/Main_Page">
+            Wikemedia Commons
+          </a>{" "}
+          and are in the public domain
         </p>
-    </div> </div>);
+      </div>{" "}
+    </div>
+  );
 }
 
-function AuthQuiz ({turnData}){
-    return (
-      <div className="container-fluid">
-        <Hero />
-        <Turn {...turnData}/>
-        <Continue />
-        <Footer />
-      </div>
-    );
-  }
+function AuthQuiz({ turnData }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
 
 export default AuthQuiz;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AuthQuiz from './AuthQuiz';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AuthQuiz from "./AuthQuiz";
+import { shuffle, sample } from "underscore";
+import * as serviceWorker from "./serviceWorker";
 
 const authors = [
   {
-    name: 'Mark Twain',
-    imageUrl: 'images/authors/marktwain.jpg',
-    imageSource: 'wikimedia commons',
-    books: ['The Adventures of Huckelberry Finn']
-  }
-]
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"],
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.png",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"],
+  },
+  {
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Daniel Ogren",
+    books: ["Harry Potter and the Sorcerers Stone"],
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Pinguino",
+    books: ["The Shining", "IT"],
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"],
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"],
+  },
+];
 
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) => title === answer)
+    ),
+  };
+}
 const state = {
   turnData: {
-  author: authors[0],
-  books: authors[0]}
+    turnData: getTurnData(authors),
+  },
 };
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthQuiz {...state} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
